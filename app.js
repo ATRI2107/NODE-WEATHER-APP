89ca5a85ff04872f26020ff2849e4f3efa744c8b
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-05-27 17:31:26 +0530

    Added json:true to request

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const request=require('request-promise');
 const url='http://api.weatherstack.com/current?access_key=f7075349ca9fe975d972786a45722ac8&query=26.8467,80.9462';
 
-request({url: url})
+const options={
+    url: url,
+    json: true
+};
+request(options)
     .then(response=>{
-        const data=JSON.parse(response);
-        console.log(data.location);
+        console.log(response.current);
     })
     .catch(err=>{
         console.log(err);
