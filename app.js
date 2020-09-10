b5d666e47d4c28ae24cb5ca7c590cc88b7fccfb2
ATRI2107 <atrichaturvedi@gmail.com>
2020-09-10 19:42:24 +0530

    App file configured

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
+const express=require('express');
+const bodyparser=require('body-parser');
+const morgan=require('morgan');
 const request=require('request-promise');
 const url='http://api.weatherstack.com/current?access_key=f7075349ca9fe975d972786a45722ac8&query=26.8467,80.9462';
 
-const options={
-    url: url,
-    json: true   //Automatically understands that the data which is coming is json
-};
-request(options)
-    .then(response=>{
-        console.log(response.current);
-    })
-    .catch(err=>{
-        console.log(err);
-    });
+const app=express();
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+app.use(morgan('dev'));
 
-const geocodintg='https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXRyaTIxMDciLCJhIjoiY2thcGYxbXdmMDljODJxbXNmZ21wM3k3YyJ9.n-Lsy9wDdf46KtH_raUwRA&limit=1';
-request({url: geocodintg,json: true})
-    .then(response=>{
-        const latitude=response.features[0].center[1];
-        const longitude=response.features[0].center[0];
-        console.log(latitude+" "+longitude);
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+module.exports=app;
