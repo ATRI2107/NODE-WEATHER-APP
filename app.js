21ae0d59d5d7df3d48ed9c7bb210697b8f3db361
ATRI2107 <atrichaturvedi@gmail.com>
2020-09-25 21:23:58 +0530

    Configured to use partials

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express=require('express');
 const path=require('path');
 const bodyparser=require('body-parser');
 const morgan=require('morgan');
+const hbs=require('hbs');
 const request=require('request-promise');
 const url='http://api.weatherstack.com/current?access_key=f7075349ca9fe975d972786a45722ac8&query=26.8467,80.9462';
+const partialsPath=path.join(__dirname,'/views/partials');
 
 const app=express();
 app.use(bodyparser.urlencoded({extended: false}));
@@ -11,7 +13,8 @@ app.use(bodyparser.json());
 app.use(morgan('dev'));
 app.set('view engine','hbs');
 app.use(express.static(path.join(__dirname,'public')));
-
+hbs.registerPartials(partialsPath);
 const indexRoute=require('./routes/index');
+const { dirname } = require('path');
 app.use("/",indexRoute);
 module.exports=app;
