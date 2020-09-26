90a1a3c988ed0d1e3f464044fdbda61d0a032883
ATRI2107 <atrichaturvedi@gmail.com>
2020-09-26 21:49:07 +0530

    Updated the routes file to call the APi

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const router=express.Router();
+const request=require('request-promise');
+var url='http://api.weatherstack.com/current?access_key=f7075349ca9fe975d972786a45722ac8&query=';
 
 router.get("/",(req,res,next)=>{
     res.render('index');
@@ -9,5 +11,29 @@ router.get("/about",(req,res)=>{
 });
 router.get("/help",(req,res)=>{
     res.render("help");
-})
+});
+router.post("/",(req,res)=>{
+    var city=req.body.city.replace(" ","%20");
+    url=url+city;
+    var options={
+        uri: url,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true
+    };
+    request(options)
+    .then(response=>{
+        const result={
+            name: city.replace("%20"," "),
+            time: response.current.observation_time,
+            temp: response.current.temperature,
+            humidity: response.current.humidity
+        };
+        res.render('weather',result);
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+});
 module.exports=router;
