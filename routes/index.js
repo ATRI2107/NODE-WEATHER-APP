d8ebe56a037bd3ba7591df21fa8ef773b7a7d998
ATRI2107 <atrichaturvedi@gmail.com>
2020-09-26 19:12:03 +0530

    Added more routes

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,10 @@ const router=express.Router();
 router.get("/",(req,res,next)=>{
     res.render('index');
 });
+router.get("/about",(req,res)=>{
+    res.render("about");
+});
+router.get("/help",(req,res)=>{
+    res.render("help");
+})
 module.exports=router;
