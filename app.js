72cd2e3a531c4d3011ac083f585c853c1400d393
ATRI2107 <atrichaturvedi@gmail.com>
2020-09-26 21:49:53 +0530

    Changed the app file

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const path=require('path');
 const bodyparser=require('body-parser');
 const morgan=require('morgan');
 const hbs=require('hbs');
-const request=require('request-promise');
-const url='http://api.weatherstack.com/current?access_key=f7075349ca9fe975d972786a45722ac8&query=26.8467,80.9462';
 const partialsPath=path.join(__dirname,'/views/partials');
 
 const app=express();
